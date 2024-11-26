0f740a1419b5a4c295f49eb28d809dcca48a4fd2
Karina Cristo <karinacristonegocios@gmail.com>
2024-11-26 20:34:00 -0300

    Fix CSP to allow Google Fonts and styles

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const authRoutes = require('./routes/authRoutes'); // Importa as rotas de autenticação
+const helmet = require('helmet'); // Importa o helmet
+const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 
 dotenv.config();
-
 const app = express();
 
+// Configurações do Helmet para permitir fontes e estilos externos
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'", 'https://fonts.googleapis.com'],
+        fontSrc: ["'self'", 'https://fonts.gstatic.com'],
+      },
+    },
+  })
+);
+
 // Middleware
-app.use(express.json()); // Para analisar requisições com JSON
+app.use(express.json());
 
 // Conexão com o banco de dados
 mongoose.connect(process.env.MONGODB_URI)
@@ -18,7 +30,7 @@ mongoose.connect(process.env.MONGODB_URI)
   .catch((error) => console.error("Erro na conexão com o MongoDB", error));
 
 // Rotas
-app.use('/api/auth', authRoutes); // Inclui a rota de login
+app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
 // Iniciar servidor
